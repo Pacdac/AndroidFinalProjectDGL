319deca7ff13538b4c35d0fb2b10087a8afe6453
Pacdac <lifra05@yahoo.fr>
2021-11-24 11:07:44 +0100

    test API camera

--- app/src/main/java/com/example/androidprojectdgl/MainActivity.java
+++ app/src/main/java/com/example/androidprojectdgl/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.androidprojectdgl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dispatchTakePictureIntent();
+    }
+
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
     }
 }
